6972f03150801f5b40942db56b30cb0043468532
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-03-18 12:33:28 -0300

    windows export

--- src/player.h
+++ src/player.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#ifdef _WIN32
+#  ifdef MODULE_API_EXPORTS
+#    define export __declspec(dllexport)
+#  else
+#    define export __declspec(dllimport)
+#  endif
+#else
+#  define export
+#endif
+
 #include "decoder.h"
 #include "framequeue.h"
 
@@ -17,44 +27,44 @@ typedef struct MediaPlayerContext {
     int buffering;
 } MediaPlayerContext;
 
-MediaPlayerContext *player_create(const char *url);
+export MediaPlayerContext *player_create(const char *url);
 
-void player_destroy(MediaPlayerContext *vpc);
+export void player_destroy(MediaPlayerContext *vpc);
 
-void player_play(MediaPlayerContext *vpc);
+export void player_play(MediaPlayerContext *vpc);
 
-void player_stop(MediaPlayerContext *vpc);
+export void player_stop(MediaPlayerContext *vpc);
 
-int player_is_buffering(MediaPlayerContext *vpc);
+export int player_is_buffering(MediaPlayerContext *vpc);
 
-int player_is_playing(MediaPlayerContext *vpc);
+export int player_is_playing(MediaPlayerContext *vpc);
 
-void player_set_paused(MediaPlayerContext *vpc, int paused);
+export void player_set_paused(MediaPlayerContext *vpc, int paused);
 
-void player_set_loop(MediaPlayerContext *vpc, int loop);
+export void player_set_loop(MediaPlayerContext *vpc, int loop);
 
-int player_has_loop(MediaPlayerContext *vpc);
+export int player_has_loop(MediaPlayerContext *vpc);
 
-float player_get_length(MediaPlayerContext *vpc);
+export float player_get_length(MediaPlayerContext *vpc);
 
-float player_get_playback_position(MediaPlayerContext *vpc);
+export float player_get_playback_position(MediaPlayerContext *vpc);
 
-void player_seek(MediaPlayerContext *vpc, float time);
+export void player_seek(MediaPlayerContext *vpc, float time);
 
-void player_process(MediaPlayerContext *vpc);
+export void player_process(MediaPlayerContext *vpc);
 
-double player_grab_video_frame(MediaPlayerContext *vpc, void **release_ptr, uint8_t **data);
+export double player_grab_video_frame(MediaPlayerContext *vpc, void **release_ptr, uint8_t **data);
 
-double player_grab_audio_frame(MediaPlayerContext *vpc, void **release_ptr, uint8_t **data, int *frame_size);
+export double player_grab_audio_frame(MediaPlayerContext *vpc, void **release_ptr, uint8_t **data, int *frame_size);
 
-void player_release_frame(MediaPlayerContext *vpc, void *release_ptr);
+export void player_release_frame(MediaPlayerContext *vpc, void *release_ptr);
 
-void player_get_video_format(MediaPlayerContext *vpc, int *width, int *height);
+export void player_get_video_format(MediaPlayerContext *vpc, int *width, int *height);
 
-void player_get_audio_format(MediaPlayerContext *vpc, int *frequency, int *channels);
+export void player_get_audio_format(MediaPlayerContext *vpc, int *frequency, int *channels);
 
-double player_get_start_time(MediaPlayerContext *vpc);
+export double player_get_start_time(MediaPlayerContext *vpc);
 
-void player_set_start_time(MediaPlayerContext *vpc, double time);
+export void player_set_start_time(MediaPlayerContext *vpc, double time);
 
-double player_get_global_time(MediaPlayerContext *vpc);
+export double player_get_global_time(MediaPlayerContext *vpc);
